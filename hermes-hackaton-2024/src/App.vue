a47bdb55b651defad9399e9378199118e2048692
Giuco <danicodes45@gmail.com>
2024-11-02 12:00:11 +0200

    Added glitch effect library, aded Router fmm Bradu

--- hermes-hackaton-2024/src/App.vue
+++ hermes-hackaton-2024/src/App.vue
@@ -1,18 +1,30 @@
 <template>
-  <main>
+  <main class="">
     <Navbar />
+    <RouterView />
     <div class="container">
-      <img src="./assets/imgs/cet.jpg" alt="Graphic" />
+      <img
+        @mouseover="makeGlitch"
+        class="glitch"
+        src="./assets/imgs/cet.jpg"
+        alt="Graphic"
+      />
     </div>
   </main>
 </template>
 
 <script setup>
+import { PowerGlitch } from 'powerglitch'
 import Navbar from './components/Navbar.vue'
 import useSponsors from './composables/useSponsors'
+import { RouterView } from 'vue-router'
 
 const { sponsors } = useSponsors()
 console.log(sponsors)
+
+const makeGlitch = () => {
+  PowerGlitch.glitch('.glitch')
+}
 </script>
 
 <style scoped>
--- hermes-hackaton-2024/src/components/Navbar.vue
+++ hermes-hackaton-2024/src/components/Navbar.vue
@@ -1,29 +1,33 @@
 <template>
-    <h1>Navbar</h1>
-    <nav>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </nav>
+  <h1>Navbar</h1>
+  <nav>
+    <ul>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/register">Register</RouterLink>
+    </ul>
+  </nav>
 </template>
 
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
 <style scoped>
-    nav {
-        background-color: #333;
-        color: white;
-        padding: 1rem;
-    }
+nav {
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+}
 
-    ul {
-        display: flex;
-        justify-content: space-around;
-        list-style: none;
-    }
+ul {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+}
 
-    a {
-        color: white;
-        text-decoration: none;
-    }
+a {
+  color: white;
+  text-decoration: none;
+}
 </style>
