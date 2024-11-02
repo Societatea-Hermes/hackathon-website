22f3fd071b0ecb0a5b93ebf617414ba5f38add95
Giuco <danicodes45@gmail.com>
2024-11-02 15:43:40 +0200

    Added buttons navbar

--- hermes-hackaton-2024/src/components/NavigationBar.vue
+++ hermes-hackaton-2024/src/components/NavigationBar.vue
@@ -1,14 +1,17 @@
 <template>
   <nav>
-    <ul>
-      <div class="nav-link-container">
-        <RouterLink class="nav-link" to="/">Home</RouterLink>
-      </div>
+    <ul class="navbar">
       <div class="nav-link-container">
         <RouterLink class="nav-link" to="/about">About</RouterLink>
       </div>
       <div class="nav-link-container">
         <RouterLink class="nav-link" to="/register">Register</RouterLink>
+      </div>
+      <div class="nav-link-container">
+        <a href="#sponsors" class="nav-link">Sponsors</a>
+      </div>
+      <div class="nav-link-container">
+        <a href="#contact" class="nav-link">Contact</a>
       </div>
     </ul>
   </nav>
@@ -31,17 +34,33 @@ ul {
   list-style: none;
 }
 
-a {
-  color: white;
-  text-decoration: none;
+.navbar {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
 }
 
 .nav-link {
-  padding: 0.5rem 1rem;
-  border-radius: 5px;
+  color: black;
+  font-weight: bold;
+  font-size: xx-large;
+  align-self: center;
+  margin-bottom: 30px;
 }
 
 .nav-link-container {
-  background-color: var(--magenta);
+  width: 300px;
+  height: 200px;
+  justify-content: center;
+  display: flex;
+  background: url('../assets/imgs/nav-unpressed.svg') center no-repeat;
+}
+
+.nav-link-container:hover {
+  background: url('../assets/imgs/nav-pressed.svg') center no-repeat;
+}
+
+.nav-link-container:active {
+  background: url('../assets/imgs/nav-pressed.svg') center no-repeat;
 }
 </style>
